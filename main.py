a9c4a8114cb5d4428cc462e031b3186d82e74ba2
Your Name <you@example.com>
2026-09-21 18:05:19 +0300

    Add main entry point

---
+++ main.py
@@ -0,0 +1,10 @@
+import lib
+
+def main():
+    # Викликаємо функцію з модуля lib та виводимо результат
+    area = lib.calculate_area(5, 10)
+    print("Площа дорівнює:", area)
+
+if __name__ == "__main__":
+    main()
+  
