c9e7766e09ecc63abfe1bbdfe5d2e2c30d645b77
Your Name <you@example.com>
2026-09-21 18:13:27 +0300

    Update print message

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import lib
 def main():
     # Викликаємо функцію з модуля lib та виводимо результат
     area = lib.calculate_area(5, 10)
-    print("Площа дорівнює:", area)
+    print("Результат обчислення площі:", area)
 
 if __name__ == "__main__":
     main()
